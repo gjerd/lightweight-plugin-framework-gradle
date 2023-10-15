cb8b624bd0ee39ef9c596d8be319da8b4fe06c2e
hectic <arild@javacentric.org>
2023-10-15 16:08:00 +0200

    Updated Licence

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # Lightweight Plugin Framework for Gradle
 
+![Release](https://badgen.net/badge/Release/0.0.1-SNAPSHOT/blue)
+[![License](https://badgen.net/badge/License/Apache&nbsp;2.0/blue)](https://opensource.org/licenses/Apache-2.0)
+
 This is early stage of using https://github.com/gjerd/lightweight-plugin-framework
 in gradle project
+
+
+
+## Licence
+Copyright 2017 2023- Arild G. Gjerd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
--- gradle-plugin-app/src/main/java/org/javacentric/App.java
+++ gradle-plugin-app/src/main/java/org/javacentric/App.java
@@ -1,19 +1,66 @@
 package org.javacentric;
 
-import ljpf.DefaultPluginManager;
+import ljpf.PluginClassLoaderFactory;
 import ljpf.PluginManager;
-import ljpf.PluginRepository;
+import ljpf.examples.common.ExampleConfig;
+import ljpf.DefaultPluginManager;
+import ljpf.loader.ParentLastClassLoaderFactory;
+import ljpf.repository.ClasspathPluginRepository;
 import ljpf.repository.DirPluginRepository;
+import ljpf.repository.MultiPluginRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+
+/**
+ * Created by souzen on 29.03.2017.
+ * Modified to suit my fork of the framework by Arild G. Gjerd
+ */
 public class App {
-    public void init() {
-        PluginRepository pluginRepository = new DirPluginRepository("/home/hectic/spades/plugins");
-        PluginManager pluginManager = new DefaultPluginManager(pluginRepository);
 
-        pluginManager.load("yay-plugin");
+    private static final Logger LOG = LoggerFactory.getLogger(App.class.getSimpleName());
+
+    private PluginManager pluginManager;
+
+    // Relative to user home directory
+    String pluginRepository1 =  System.getProperty("user.home") +
+                                File.separator + "spades" +
+                                File.separator + "plugins";
+
+    // Relative to project-directory
+    String pluginRepository2 =  "plugins";
+
+    public void init() {
+
+        new File(pluginRepository2).mkdirs();
+
+        MultiPluginRepository pluginRepository = new MultiPluginRepository(
+                new ClasspathPluginRepository(),
+                new DirPluginRepository(pluginRepository1),
+                new DirPluginRepository(pluginRepository2)
+        );
+
+        PluginClassLoaderFactory classLoaderFactory = new ParentLastClassLoaderFactory();
+        pluginManager = new DefaultPluginManager(pluginRepository, classLoaderFactory);
+
+        ExampleConfig config = new ExampleConfig();
+        config.setValue("World");
+
+        pluginManager.load("FirstPlugin", config);
+        //pluginManager.load("SecondPlugin", config);
+        //pluginManager.load("ThirdPlugin", config);
+    }
+
+    public void shutdown() {
+        LOG.info("Shutting down...");
+        pluginManager.unloadAll();
     }
 
     public static void main(String[] args) {
-        new App().init();
+        App app = new App();
+        app.init();
+        app.shutdown();
     }
+
 }
